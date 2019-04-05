dc7924d863dcb0dcafc892b6025debc4623344c8
王于哲 <ycw1009@gmail.com>
2019-04-06 02:23:29 +0800

    Debug forward checking

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@
 #include <fstream>
 #include <unordered_map>
 #include <climits>
+#include <stack>
 using namespace std;
 
 class Word{
 	public:
+		int num;
 		int len;
 		int startX, startY;
 		char direction;
@@ -65,12 +67,15 @@ class Constraint{//Binary constraint
 
 vector<string> vocabulary[15];								//the length of the longest vocabulary
 unordered_map<Word, string > assignments;
+stack<Word> assign_stack;
 int node_expand =0;
 int answer_cnt = 0;
 class Puzzle{
 	public:
 		vector<Word> words;//Variables
 		unordered_map<Word, vector<string> > domains;
+		unordered_map<Word, vector<string> > old_domains;//use to reset domain to ussign state
+		unordered_map<Word, unordered_map<Word, vector<string> > > fc_state; //use to reset other domain to ussign state
 		unordered_map<Word, vector<Constraint> > neighbors;
 		
 
@@ -80,7 +85,7 @@ class Puzzle{
 			int startX, startY, len;
 			char direction;
 			while(ss>>startX>>startY>>len>>direction){
-				Word word(startX,startY,len,direction);
+				Word word( startX,startY,len,direction);
 				this->words.push_back(word);
 			}
 		}
@@ -88,8 +93,8 @@ class Puzzle{
 		void setDomain(){
 			for(Word& word: this->words){
 				this->domains[word] = vocabulary[word.len];
-				
 			}
+
 		}
 		void setConstraint(){
 			for(int i = 0; i < this->words.size(); i ++){
@@ -119,57 +124,86 @@ class Puzzle{
 			
 		}
 		bool assign(Word word, string str, bool fc = false){
+			this->old_domains[word] = this->domains[word];
+			vector<string> assign_val = {str};
+			this->domains[word] = assign_val;
 			assignments[word] = str;
 			//cout<<"Assign " <<str<< " Successfully"<<endl;
 			if(fc){
+				this->fc_state[word] = this->domains;
+				this->fc_state[word][word] = this->old_domains[word];
 				for(Constraint c: this->neighbors[word]){
 					auto got = assignments.find(c.to);
-					if(got == assignments.end()) continue;// this neighbor doesn't have value yet
-
+					if(got != assignments.end()) continue;// this neighbor has word but checked already
 					for(auto i = this->domains[c.to].begin(); i != this->domains[c.to].end();){//iterate all neighbors domain 
 						if((*i)[c.to_index]!= str[c.from_index])//remove illegal ones 
 						{	
-							domains[c.to].erase(i);
+							this->domains[c.to].erase(i);
 						}
 						else ++i;
 					}
-					if(this->domains[c.to].empty()) return false;
-					
+					if(this->domains[c.to].empty()) {	
+						return false;
+							
+					}
 				}
 
 			}
 			return true;
 		}
 
-		void unassign(Word word){
+		void unassign(Word word, bool fc = false){
 			auto got = assignments.find(word);
 			if(got == assignments.end()) return;
 			assignments.erase(word);
 			//reset domain of unassign word
-			domains[word] = vocabulary[word.len];
+			if(fc) this->domains = this->fc_state[word];
+			else this->domains[word] = this->old_domains[word];
 			//printf("Unassign Successfully\n");
 
 		}
 		
 		Word selectUnsignedWord(bool MRV = false, bool DEGREE = false){
-			if (DEGREE){
+			if (MRV && DEGREE){//apply mrv first
+				Word return_word;
+				int least_domain_num = INT_MAX;
+				int most_degree_cnt = 0;
+				for(int i = 0; i <this->words.size(); i++){
+					auto got = assignments.find(this->words[i]);
+					if(got == assignments.end() && this->domains[this->words[i]].size() < least_domain_num){
+						return_word = this->words[i];
+						least_domain_num = this->domains[this->words[i]].size();
+					}
+					else if(got == assignments.end() && this->domains[this->words[i]].size() == least_domain_num){
+						if(this->neighbors[this->words[i]].size() > most_degree_cnt){
+							return_word = this->words[i];
+							least_domain_num = this->domains[this->words[i]].size();
+						}
+					}
+
+				}
+				return return_word;
+			}
+			else if (DEGREE){
 				Word degree_max_word;
 				int most_degree_cnt = 0;
 				for(int i = 0; i <this->words.size(); i++){
 					auto got = assignments.find(this->words[i]);
-					if(got == assignments.end() && this->neighbors[this->words[i]].size() >= most_degree_cnt){
+					if(got == assignments.end() && this->neighbors[this->words[i]].size() > most_degree_cnt){
 						degree_max_word = this->words[i];
 						most_degree_cnt = this->neighbors[this->words[i]].size();
 					}
 				}
 				return degree_max_word;
 			}
-			if (MRV){
+
+			else if (MRV){
+
 				Word mini_remain_word;
 				int least_domain_num = INT_MAX;
 				for(int i = 0; i <this->words.size(); i++){
 					auto got = assignments.find(this->words[i]);
-					if(got == assignments.end() && this->domains[this->words[i]].size()<=least_domain_num){
+					if(got == assignments.end() && this->domains[this->words[i]].size() <= least_domain_num){
 						mini_remain_word = this->words[i];
 						least_domain_num = this->domains[this->words[i]].size();
 					}
@@ -188,8 +222,8 @@ class Puzzle{
 		bool nonConflict(Word select, string str){
 			for(Constraint& c :this->neighbors[select]){
 				auto got = assignments.find(c.to);
-				if(got == assignments.end()) continue;// this neighbor doesn't have value yet
-				if(str[c.from_index] != assignments[c.to][c.to_index])// different char in the same position
+				if(got == assignments.end()) {}// this neighbor doesn't have value yet
+				else if(str[c.from_index] != assignments[c.to][c.to_index])// different char in the same position
 					return false;
 			}
 			return true;
@@ -197,29 +231,31 @@ class Puzzle{
 
 
 };
-bool recursiveBackTracking(Puzzle P,bool FC = false, bool MRV = false, bool DEGREE = false, bool show_all = false){
+bool recursiveBackTracking(Puzzle& P,bool FC = false, bool MRV = false, bool DEGREE = false, bool show_all = false){
 	if (assignments.size() == P.words.size()) return true;
-	else{
-		
+	else{	
 		Word select = P.selectUnsignedWord(MRV, DEGREE);
 		for(string str: P.domains[select]){
 			if(P.nonConflict(select, str)) {
-				node_expand++;
 				if (!P.assign(select,str,FC)){
-					P.unassign(select);
+					P.unassign(select,FC);
+					continue;
 				}
-				bool result = recursiveBackTracking(P, FC, MRV, show_all);
-				if(result && assignments.size() == P.words.size() && show_all){
+				node_expand++;
+				bool result = recursiveBackTracking(P, FC, MRV, DEGREE, show_all);
+				if(result && !show_all) return result;
+				else if (result && show_all) {
 					answer_cnt++;
 				}
-				else if(result) return result;
-				P.unassign(select);
+				P.unassign(select,FC);
+
 			}
 		}
+		
 		return false;
 	}
 }
-void backTracking (Puzzle P,bool FC = false, bool MRV = false, bool DEGREE = false, bool show_all = false){
+void backTracking (Puzzle& P,bool FC = false, bool MRV = false, bool DEGREE = false, bool show_all = false){
 	if(recursiveBackTracking(P, FC, MRV, DEGREE, show_all)){
 		printf("Answer Found\n");
 		for ( auto it = assignments.begin(); it != assignments.end(); ++it ){
@@ -236,21 +272,22 @@ void backTracking (Puzzle P,bool FC = false, bool MRV = false, bool DEGREE = fal
 
 int main(){
 	bool FC = false;  // Apply forward checking
-	bool MRV = false; // Apply Minimum Remaining Variable selection
+	bool MRV = true;; // Apply Minimum Remaining Variable selection
 	bool DEGREE = false;// Apply most neighbors Variable selection
-	bool show_all = false;// Find an answer and stop or go through all possible answers
+	bool show_all = true;// Find an answer and stop or go through all possible answers
 	ifstream fin2("English Words 3000.txt");
+	ifstream fin("puzzle.txt");
+	if (!fin||!fin2) {
+		cout << "Error opening file. Shutting down..." << endl;
+		return 0;
+	}
 	for(string str; getline( fin2, str ,'\n'); ){ // store all vocabulary in vector
 		stringstream ss(str); // remove weird characters
 		string str2;
 		ss>>str2;
 		vocabulary[str2.size()].push_back(str2);
 	}
-	ifstream fin("puzzle.txt");
-	if (!fin) {
-		cout << "Error opening file. Shutting down..." << endl;
-		return 0;
-	}
+	
 	for(string str; getline( fin, str ,'\n'); ){
 		Puzzle P(str);			 								// initial puzzle with input string
 		P.setDomain();											// set domain initially with corresponding length of vocabulary
